e28972cc928a0d74ebfe61d8206e6545cbbf81d9
sutogabor <wakacorpz@gmail.com>
2022-12-15 10:43:55 +0100

    add constructor and addTask method to TaskService class

--- MentorMeSpring/src/main/java/com/codecool/MentorMe/service/TaskService.java
+++ MentorMeSpring/src/main/java/com/codecool/MentorMe/service/TaskService.java
@@ -1,4 +1,25 @@
 package com.codecool.MentorMe.service;
 
+import com.codecool.MentorMe.model.Task;
+import com.codecool.MentorMe.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Random;
+
+@Service
 public class TaskService {
+    private final TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository tr) {
+        this.taskRepository = tr;
+    }
+
+    public void addTask(Task task) {
+        taskRepository.save(task);
+    }
+
+
 }
