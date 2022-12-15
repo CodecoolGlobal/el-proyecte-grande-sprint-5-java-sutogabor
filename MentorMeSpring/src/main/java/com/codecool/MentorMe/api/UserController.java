b5077fd34534a0d14b36aa0bf251e65fe1a7eb53
sutogabor <wakacorpz@gmail.com>
2022-12-15 10:13:48 +0100

    add constructor and getAllUsers method to UserController class

--- MentorMeSpring/src/main/java/com/codecool/MentorMe/api/UserController.java
+++ MentorMeSpring/src/main/java/com/codecool/MentorMe/api/UserController.java
@@ -1,4 +1,24 @@
 package com.codecool.MentorMe.api;
 
+import com.codecool.MentorMe.model.User;
+import com.codecool.MentorMe.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 public class UserController {
+    private UserService us;
+
+    @Autowired
+    public UserController(UserService us) {
+        this.us = us;
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return us.getAllUsers();
+    }
+
+
 }
