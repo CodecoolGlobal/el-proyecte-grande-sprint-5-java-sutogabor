85933570537d2ab0a3a312afa63eadbb46a2a837
sutogabor <wakacorpz@gmail.com>
2022-12-15 10:33:57 +0100

    add constructor, getAnswerById and addAnswer methods to AnswerService class

--- MentorMeSpring/src/main/java/com/codecool/MentorMe/service/AnswerService.java
+++ MentorMeSpring/src/main/java/com/codecool/MentorMe/service/AnswerService.java
@@ -1,4 +1,19 @@
 package com.codecool.MentorMe.service;
 
+import com.codecool.MentorMe.model.Answer;
+import com.codecool.MentorMe.repository.AnswerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AnswerService {
+    private final AnswerRepository answerRepository;
+
+    @Autowired
+    public AnswerService(AnswerRepository answerRepository) {
+        this.answerRepository = answerRepository;
+    }
+
+    public Answer getAnswerById(Long id) { return answerRepository.getById(id); }
+    public void addAnswer(Answer task) {
+        answerRepository.save(task);
+    }
 }
