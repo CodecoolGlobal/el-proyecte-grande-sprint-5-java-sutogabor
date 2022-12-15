391a0756a9c7025d03e8d739855f7ab18997614a
sutogabor <wakacorpz@gmail.com>
2022-12-15 10:17:19 +0100

    add fields and annotations to Answer class

--- MentorMeSpring/src/main/java/com/codecool/MentorMe/model/Answer.java
+++ MentorMeSpring/src/main/java/com/codecool/MentorMe/model/Answer.java
@@ -1,4 +1,34 @@
 package com.codecool.MentorMe.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "answers")
 public class Answer {
+    @SequenceGenerator(
+            name = "answer_sequence",
+            sequenceName = "answer_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "answer_sequence"
+    )
+    @JsonIgnore
+    @Id
+    private Long id;
+    private String message;
+
+    @JsonIgnore
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "task_id")
+    private Task taskOptions;
 }
