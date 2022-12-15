3d20bba6b4426346e6b82da0da1cb428c7a6fb4e
sutogabor <wakacorpz@gmail.com>
2022-12-15 10:09:42 +0100

    add constructor and addTask method to TaskController class

--- MentorMeSpring/src/main/java/com/codecool/MentorMe/api/TaskController.java
+++ MentorMeSpring/src/main/java/com/codecool/MentorMe/api/TaskController.java
@@ -1,4 +1,20 @@
 package com.codecool.MentorMe.api;
 
+import com.codecool.MentorMe.model.Task;
+import com.codecool.MentorMe.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 public class TaskController {
+    private TaskService taskService;
+    @Autowired
+    public TaskController(TaskService ts) {
+        this.taskService = ts;
+    }
+
+    @PostMapping
+    public void addTask(@RequestBody Task newTask) {
+        taskService.addTask(newTask);
+    }
+
 }
