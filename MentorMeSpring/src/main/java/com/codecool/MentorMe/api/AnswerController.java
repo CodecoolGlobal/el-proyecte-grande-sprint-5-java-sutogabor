49f5d94defa63d2cec45ada854bd370210c3517a
sutogabor <wakacorpz@gmail.com>
2022-12-15 10:05:59 +0100

    add constructor and checkAnswer method to AnswerController class

--- MentorMeSpring/src/main/java/com/codecool/MentorMe/api/AnswerController.java
+++ MentorMeSpring/src/main/java/com/codecool/MentorMe/api/AnswerController.java
@@ -1,4 +1,23 @@
 package com.codecool.MentorMe.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 public class AnswerController {
+
+    private final AnswerService answerService;
+    private final SolutionService solutionService;
+
+    @Autowired
+    public AnswerController(AnswerService as, SolutionService solutionService) {
+        this.answerService = as;
+        this.solutionService = solutionService;
+    }
+
+    @GetMapping("/{id}")
+    public boolean checkAnswer(@PathVariable("id") Long id) {
+        return solutionService.checkAnswer(id);
+    }
+
 }
