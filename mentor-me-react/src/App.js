0dbf85f8bdf11777457c0b67b833e31cec41f556
Kitti Born <born.kitti97@gmail.com>
2022-12-15 10:14:04 +0100

    add home component

--- mentor-me-react/src/App.js
+++ mentor-me-react/src/App.js
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import {useState} from "react";
 import Footer from "./Footer";
+import Home from "./Home";
 
 function App() {
     const [isLogged, setIsLogged] = useState(true);
@@ -12,6 +13,10 @@ function App() {
             title="Mentor Me"
             isLogged={isLogged}
         />
+        { isLogged
+            ? <div>Logged in</div>
+            : <Home />
+        }
         <Footer
             text="Coodecool 2022"
         />
---
+++ mentor-me-react/src/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home = () => {
+    return <div>
+        <h1>This is the Home page</h1>
+        <p>This page is shown when User is not logged in.</p>
+    </div>
+}
+
+export default Home;
