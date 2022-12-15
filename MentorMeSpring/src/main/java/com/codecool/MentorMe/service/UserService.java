db1b4c011d8f073144294e69c653246193dcadb6
sutogabor <wakacorpz@gmail.com>
2022-12-15 10:48:12 +0100

    add constructor and addUser method to UserService class

--- MentorMeSpring/src/main/java/com/codecool/MentorMe/service/UserService.java
+++ MentorMeSpring/src/main/java/com/codecool/MentorMe/service/UserService.java
@@ -1,4 +1,23 @@
 package com.codecool.MentorMe.service;
 
+import com.codecool.MentorMe.model.User;
+import com.codecool.MentorMe.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
 public class UserService {
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository ur) {
+        this.userRepository = ur;
+    }
+
+    public void addUser(User user) {
+        userRepository.save(user);
+    }
+
+
 }
