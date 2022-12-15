cbef0d7d6a7fba453a37dd4d0d6105274a00b1aa
sutogabor <wakacorpz@gmail.com>
2022-12-15 10:35:44 +0100

    add constructor, checkAnswer and addSolution methods to SolutionService class

--- MentorMeSpring/src/main/java/com/codecool/MentorMe/service/SolutionService.java
+++ MentorMeSpring/src/main/java/com/codecool/MentorMe/service/SolutionService.java
@@ -1,4 +1,22 @@
 package com.codecool.MentorMe.service;
 
+import com.codecool.MentorMe.model.Solution;
+import com.codecool.MentorMe.repository.SolutionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class SolutionService {
+    private final SolutionRepository solutionRepository;
+
+    @Autowired
+    public SolutionService(SolutionRepository solutionRepository) {
+        this.solutionRepository = solutionRepository;
+    }
+
+    public boolean checkAnswer(Long id) {
+        return solutionRepository.getById(id).isCorrect();
+    }
+
+    public void addSolution(Solution solution) {
+        solutionRepository.save(solution);
+    }
 }
